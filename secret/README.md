ef904aa5f81bf8b4d99ed86cf11e51c6c31f7cd0
Tristan Chin <tristan.chin@chintristan.io>
2021-12-16 02:21:57 -0500

    Finish CIBCFrench

--- secret/README.md
+++
@@ -1,3 +0,0 @@
-This folder should contain the following files:
-
-- statement.pdf - The bank statement. Probably only works with CIBC statements.
--- src/classes/parsers/CIBCFrench.ts
+++ src/classes/parsers/CIBCFrench.ts
@@ -1,5 +1,21 @@
+import fs from "fs";
 import Transaction from "../Transaction";
 import Parser from "./Parser";
+
+const MONTHS: { [key: string]: string } = {
+    jan: "jan",
+    fév: "feb",
+    mar: "mar",
+    avr: "apr",
+    mai: "may",
+    jun: "jun",
+    jlt: "jul",
+    aoû: "aug",
+    sep: "sep",
+    oct: "oct",
+    nov: "nov",
+    déc: "dec",
+};
 
 class CIBCFrench extends Parser {
     constructor(pdfPath: string) {
@@ -8,6 +24,7 @@ class CIBCFrench extends Parser {
 
     async init(): Promise<void> {
         const { text } = await this.getFilePDF();
+        fs.writeFileSync("secret/CIBCFrench.txt", text);
         const lines = text.split("\n");
         let currentLine = 0;
 
@@ -16,36 +33,47 @@ class CIBCFrench extends Parser {
 
             // Start of a transaction list
             if (line.startsWith("No de carte ")) {
-
-                // Look for a line where the 6 first characters create a date
-                let date: Date = new Date();
-                do {
-                    line = lines[currentLine++];
-                    date = new Date(line.slice(0, 6));
-                } while (currentLine < lines.length && isNaN(date.getTime()));
-
-                if (isNaN(date.getTime())) {
-                    if (currentLine === lines.length) continue; // Got an invalid date because there are no more lines
-                    throw new Error(`"${line}" is not a valid date`);
-                }
-
-                // Look for an amount in the lines (including the one that contains the date)
-                let amount: number = NaN;
-                do {
-                    const match = line.match(/\d+\,\d{2}/);
-                    if (match) {
-                        amount = parseFloat(match[0].replace(",", "."));
-                    } else {
+                while (currentLine < lines.length) {
+                    // Look for a line where the 6 first characters create a date
+                    let date: Date = new Date();
+                    let missed = 0;
+                    do {
                         line = lines[currentLine++];
-                    }
-                } while (currentLine < lines.length && isNaN(amount));
-                
-                if (isNaN(amount)) {
-                    if (currentLine === lines.length) continue; // Got an invalid amount because there are no more lines
-                    throw new Error(`"${line}" is not a valid amount`);
-                }
+                        const m = line.substring(0, 3);
+                        const d = line.substring(4, 6);
+                        date = new Date(`${MONTHS[m] ?? m} ${d}`);
+                        missed++;
+                    } while (
+                        currentLine < lines.length &&
+                        isNaN(date.getTime()) &&
+                        missed < 3
+                    );
 
-                this.transactions.push(new Transaction(amount, date));
+                    if (missed === 3) break; // Assume we've reached the end of the transactions
+
+                    if (isNaN(date.getTime())) {
+                        if (currentLine === lines.length) continue; // Got an invalid date because there are no more lines
+                        throw new Error(`"${line}" is not a valid date`);
+                    }
+
+                    // Look for an amount in the lines (including the one that contains the date)
+                    let amount: number = NaN;
+                    do {
+                        const match = line.substring(line.length - 10).match(/\d+\,\d{2}/);
+                        if (match) {
+                            amount = parseFloat(match[0].replace(",", "."));
+                        } else {
+                            line = lines[currentLine++];
+                        }
+                    } while (currentLine < lines.length && isNaN(amount));
+
+                    if (isNaN(amount)) {
+                        if (currentLine === lines.length) continue; // Got an invalid amount because there are no more lines
+                        throw new Error(`"${line}" is not a valid amount`);
+                    }
+
+                    this.transactions.push(new Transaction(amount, date));
+                }
             }
         }
     }
